3cd2ecd7f1e6e5ce46ca034cd57c8fb949af4ef6
arabellahurrell <arabellahurrell@gmail.com>
2023-06-01 16:23:43 +0100

    multiplication done

--- src/data_processing.c
+++ src/data_processing.c
@@ -13,7 +13,6 @@ char* arithmetics(char* opcode, char* rd, char* rn, char* op2, char* shiftType, 
     } else {
         x = "0";
     }
-
     char* result = strcat(strcat(x, strcat(opcode, "100010")), strcat(master(convert(op2,18), shiftType, shiftAmount) + 10, convert(rd, 5)));
     return result;
 }
@@ -29,7 +28,7 @@ char* moveWides(char* opcode, char* rd, char* imm, char* sh, char* shiftType, ch
     return result;
 }
 
-char* logicalBitwise (char* opcode, char* rd, char* rn, char* rm, char* op2, char* shiftType) {
+char* logicalBitwise (char* mnemonic, char* rd, char* rn, char* rm, char* op2, char* shiftType) {
     char* x;
     if (strcmp((const char *) rd[0], "x") == 0) {
         x = "1";
@@ -50,15 +49,31 @@ char* logicalBitwise (char* opcode, char* rd, char* rn, char* rm, char* op2, cha
         shifts[0] = "1";
         shifts[1] = "1";
     }
-    if (strcmp(opcode, "and") ==0 || strcmp(opcode, "orr") ==0 || strcmp(opcode, "eon") ==0 || strcmp(opcode, "ands") ==0) {
+    if (strcmp(mnemonic, "and") ==0 || strcmp(mnemonic, "orr") ==0 || strcmp(mnemonic, "eon") ==0 || strcmp(mnemonic, "ands") ==0) {
         shifts[2] = "0";
     } else {
         shifts[2] = "1";
     }
-    char* result = strcat(strcat(x, strcat(opcode, strcat(strcat("01010", shifts), convert(rm, 5)))), strcat(convert(op2, 6), strcat(convert(rn, 5), convert(rd, 5))));
+    char* result = strcat(strcat(x, strcat(mnemonic, strcat(strcat("01010", shifts), convert(rm, 5)))), strcat(convert(op2, 6), strcat(convert(rn, 5), convert(rd, 5))));
     return result;
 }
 
+char* multiply (char* mnemonic, char* rd, char* rn, char* rm, char* ra) {
+    char* x;
+    if (strcmp((const char *) rd[0], "x") == 0) {
+        x = "1";
+    } else {
+        x = "0";
+    }
+    char* negate;
+    if (strcmp(mnemonic, "madd") == 0) {
+        negate = "0";
+    } else {
+        negate = "1";
+    }
+    char* result = strcat(strcat(x, strcat("0011011000", convert(rm, 5))), strcat(negate, strcat(convert(ra,5), strcat(convert(rn, 5), convert(rd, 5)))));
+    return result;
+}
 
 
 
