95bb36cded22067e5e51221a5723b3437dbc5b1a
arabellahurrell <arabellahurrell@gmail.com>
2023-06-01 15:21:01 +0100

    Wrote the shift function

--- src/data_processing.c
+++ src/data_processing.c
@@ -6,13 +6,14 @@
 #include <string.h>
 #include "denary_to_binary.c"
 
-char* arithmetics(char* opcode, char* rd, char* rn, char* op2, char* shift) {
+char* arithmetics(char* opcode, char* rd, char* rn, char* op2, char* shiftType, char* shiftAmount) {
     char* x;
     if (strcmp((const char *) rn[0], "x") == 0) {
         x = "1";
     } else {
         x = "0";
     }
+
     char* result = strcat(strcat(x, strcat(opcode, "100010")), strcat(convert(op2, 16), convert(rd, 4)));
     return result;
 }
--- src/shifts.c
+++ src/shifts.c
@@ -3,4 +3,17 @@
 //
 
 #include "shifts.h"
-
+#include <string.h>
+#include <stdlib.h>
+char* master(char* value, char* type, char* amount) {
+    uint8_t new;
+    u_int8_t val = atoi(value);
+    u_int8_t am = atoi(amount);
+    if(strcmp(type, "lsl") == 0) {
+        new = val << am;
+    } else if (strcmp(type, "lsr") == 0 || strcmp(type, "asr") == 0) {
+        new = val >> am;
+    }
+    char * result = (char*) &new;
+    return result + 2;
+}
