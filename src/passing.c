cb7e56f787fd976c0c8ad90b237ad5045dd1e06f
arabellahurrell <arabellahurrell@gmail.com>
2023-06-02 13:08:46 +0100

    started the passing

--- src/passing.c
+++ src/passing.c
@@ -3,4 +3,17 @@
 //
 
 #include "passing.h"
+#include "symbol_table.c"
+#include <string.h>
 
+char** one_pass(char** instruction) {
+    HashMap symbol_table;
+    initializeHashMap(&symbol_table);
+    char* label;
+    // first pass
+    for (int i = 0; i < sizeof(instruction)/sizeof(instruction[0]); i++) {
+        if (instruction[i][strlen(instruction[i]) - 1] == ":") {
+            //insert(symbol_table, strncpy(), i + 1)
+        }
+    }
+}
