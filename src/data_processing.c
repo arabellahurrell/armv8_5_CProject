a675217424e22b18e05c1141f75c264837fe94e9
arabellahurrell <arabellahurrell@gmail.com>
2023-06-05 15:09:54 +0100

    branching done

--- src/data_processing.c
+++ src/data_processing.c
@@ -4,38 +4,13 @@
 
 #include "data_processing.h"
 #include <string.h>
-#include "denary_to_binary.c"
+#include "utility.c"
 #include "shifts.c"
-char* arithmetics(char* opcode, char* rd, char* rn, char* op2, char* shiftType, char* shiftAmount) {
-    char* x;
-    if (strcmp((const char *) rn[0], "x") == 0) {
-        x = "1";
-    } else {
-        x = "0";
-    }
-    char* result = strcat(strcat(x, strcat(opcode, "100010")), strcat(master(convert(op2,18), shiftType, shiftAmount) + 10, convert(rd, 5)));
-    return result;
-}
 
-char* moveWides(char* opcode, char* rd, char* imm, char* sh, char* shiftType, char* shiftAmount) {
-    char* x;
-    if (strcmp((const char *) rd[0], "x") == 0) {
-        x = "1";
-    } else {
-        x = "0";
-    }
-    char* result = strcat(strcat(x, strcat(opcode, "101")), strcat(strcat(master(convert(imm, 16), shiftType, shiftAmount), sh), convert(rd, 16)));
-    return result;
-}
 
-char* logicalBitwise (char* mnemonic, char* rd, char* rn, char* rm, char* op2, char* shiftType) {
-    char* x;
-    if (strcmp((const char *) rd[0], "x") == 0) {
-        x = "1";
-    } else {
-        x = "0";
-    }
-    char* shifts[3];
+
+char* shiftBits (char* shiftType) {
+    char* shifts[2];
     if (strcmp(shiftType, "lsl") == 0) {
         shifts[0] = "0";
         shifts[1] = "0";
@@ -49,29 +24,41 @@ char* logicalBitwise (char* mnemonic, char* rd, char* rn, char* rm, char* op2, c
         shifts[0] = "1";
         shifts[1] = "1";
     }
+    return shifts;
+}
+
+char* arithmetics(char* opcode, char* rd, char* rn, char* op2, char* shiftType, char* shiftAmount) {
+
+    return strcat(strcat(sf(rn), strcat(opcode, "100010")), strcat(master(convert(op2,18), shiftType, shiftAmount) + 10, convert(rd, 5)));
+}
+
+char* registerArithmetic(char* opcode, char* rd, char* rn, char*rm, char* op2, char* shiftType, char* shiftValue) {
+    return strcat(strcat(strcat(strcat(sf(rd), opcode), "01011"), strcat(shiftBits(shiftType), "0")), strcat(strcat(rm, master(op2, shiftType, shiftValue)), strcat(convert(rn,5), convert(rd,5))));
+}
+
+char* moveWides(char* opcode, char* rd, char* imm, char* sh, char* shiftType, char* shiftAmount) {
+    return strcat(strcat(sf(rd), strcat(opcode, "101")), strcat(strcat(master(convert(imm, 16), shiftType, shiftAmount), sh), convert(rd, 16)));
+}
+
+char* logicalBitwise (char* mnemonic, char* rd, char* rn, char* rm, char* op2, char* shiftType) {
+    char* shifts = shiftBits(shiftType);
     if (strcmp(mnemonic, "and") ==0 || strcmp(mnemonic, "orr") ==0 || strcmp(mnemonic, "eon") ==0 || strcmp(mnemonic, "ands") ==0) {
-        shifts[2] = "0";
+        shifts[2] = (char) "0";
     } else {
-        shifts[2] = "1";
+        shifts[2] = (char) "1";
     }
-    char* result = strcat(strcat(x, strcat(mnemonic, strcat(strcat("01010", shifts), convert(rm, 5)))), strcat(convert(op2, 6), strcat(convert(rn, 5), convert(rd, 5))));
+    char* result = strcat(strcat(sf(rd), strcat(mnemonic, strcat(strcat("01010", shifts), convert(rm, 5)))), strcat(convert(op2, 6), strcat(convert(rn, 5), convert(rd, 5))));
     return result;
 }
 
 char* multiply (char* mnemonic, char* rd, char* rn, char* rm, char* ra) {
-    char* x;
-    if (strcmp((const char *) rd[0], "x") == 0) {
-        x = "1";
-    } else {
-        x = "0";
-    }
     char* negate;
     if (strcmp(mnemonic, "madd") == 0) {
         negate = "0";
     } else {
         negate = "1";
     }
-    char* result = strcat(strcat(x, strcat("0011011000", convert(rm, 5))), strcat(negate, strcat(convert(ra,5), strcat(convert(rn, 5), convert(rd, 5)))));
+    char* result = strcat(strcat(sf(rd), strcat("0011011000", convert(rm, 5))), strcat(negate, strcat(convert(ra,5), strcat(convert(rn, 5), convert(rd, 5)))));
     return result;
 }
 
--- src/single_data_transfer.c
+++ src/single_data_transfer.c
@@ -4,17 +4,30 @@
 
 #include "single_data_transfer.h"
 #include <string.h>
+#include "utility.c"
+#include "shifts.c"
+
 char* singleData (char* rt, char* xn, char* val, char* shiftAmount) {
-    char* x;
-    if (strcmp((const char *) rt[0], "x") == 0) {
-        x = "1";
-    } else {
-        x = "0";
-    }
-    int sizeOfValue = sizeof(val)/(sizeof(val[0]));
+    char* x = sf(rt);
+    int sizeOfValue = strlen(val);
     if (val[sizeOfValue -1] == "!") {
 
     }
 }
 
-char* registerOffset ()
+char* registerOffset(char* rt, char* xn, char* xm, char* shiftAmount, char* u, char* l) {
+    convert((char*) (binaryToDecimal(master(xm, "lsl", shiftAmount)) + binaryToDecimal(xn)), 19);
+    return strcat(strcat(strcat("1", sf(rt)), strcat("1110000", l)), strcat(strcat("1", convert(xm, 5)), strcat("011010", strcat(convert(xn, 5), convert(rt, 5)))));
+}
+
+char* indexedOffset(char* rt, char* xn, char* value, char* shiftAmount, char* u, char* l, char* i) {
+    return strcat(strcat(strcat("1", sf(rt)), strcat("1110000", l)), strcat(strcat(truncateString(value, 9), i), strcat(strcat("1", convert(xn, 5)), convert(rt, 5))));
+}
+
+char* unsignedOffset (char* rt, char* value, char* xn, char* shiftAmount, char* u, char* l) {
+    return strcat(strcat("0", strcat(sf(rt), "1110010")), strcat(strcat(l, truncateString(value, 12)), strcat(convert(xn, 5), convert(rt, 5))));
+}
+
+char* loadLiteral(char* rt, char* value) {
+    return strcat(strcat("0", sf(rt)), strcat("011000", strcat(truncateString(value, 19), convert(rt, 5))));
+}
