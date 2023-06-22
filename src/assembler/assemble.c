5254f5123ebfbdaea3c0b0be624ee45a2699095b
arabellahurrell <arabellahurrell@gmail.com>
2023-06-22 14:13:08 +0100

    ALL TESTS PASSING

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -3,7 +3,6 @@
 
 int main(int argc, char **argv) {
    const char* filename = argv[1];  // Get the filename from command line argument
-    //const char* filename = "add02.s";  // Alternatively, set the filename directly
     char** lines = readLinesFromFile(filename);  // Read lines from the specified file
     removeMultilineComments(lines, getStringArrayLength(lines));
     for (int i = 0; i < getStringArrayLength(lines); i++) {
@@ -12,7 +11,6 @@ int main(int argc, char **argv) {
     removeBlankLines(lines, getStringArrayLength(lines));
     int length = countLinesInFile(filename);
     lines[length] = NULL;
-    //one_pass(lines, "output.bin");  // Perform a one-pass assembly with modified lines
     one_pass(lines, argv[2]);  // Alternatively, use the second command line argument as the output file
 
     // The rest of the code execution continues...
